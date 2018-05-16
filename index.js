89adb808e3158dc08c912489f91fb8f0ed089058
savyounts@gmail.com <savyounts@gmail.com>
2018-05-16 05:15:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,9 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(number){
   var array =[]
-  
+  do{ array.push ('I love the Beatles!');
+    number++;
+  }
+  while (number<15)
+  return array
 }
