21726f3690b9af3a2ccabbcd34c335b3a04990a2
savyounts@gmail.com <savyounts@gmail.com>
2018-05-16 03:47:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  for (var i=0, i<4; i++){
+  for (var i=0; i<4; i++){
     musicians[i] "plays" instrument[i];
   }
   return array
