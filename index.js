b1736bf5c1693fa671bca65096b6b4a27320d1fd
savyounts@gmail.com <savyounts@gmail.com>
2018-05-16 04:13:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts){
   var i = 0
   while(i<4) {
-    var woo = !!!
+    var woo = '!!!'
     facts[i]+woo
   }
   return array
